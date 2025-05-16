9c15c0f575453eaa5a173b544439b0e608de4f6a
vetskup <veeti.pellinen@aalto.fi>
2025-05-16 18:34:22 +0300

    ScrollToTop lisätty, jolloin results-sivulle mentäessä käyttäjä aloittaa sivun ylhäältä

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { Routes, Route, Navigate} from 'react-router-dom'
 import Courses from './pages/courses'
 import Programs from './pages/programs'
 import Results from './pages/results';   // Import the new Results page
-
+import ScrollToTop from "./ScrollToTop";
 import './styles/App.css'
 
 function App() {
 
   return (
     <>
+      <ScrollToTop />
       <Routes>
         {/* automaattinen redirecti kurssisivulle */}
         <Route path="/" element={<Navigate to="/courses" replace />} />
